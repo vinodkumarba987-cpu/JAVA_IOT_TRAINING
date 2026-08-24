3a31b5a30aa1045375c3e47447017e692c13b1ff
vinodkumarba987-cpu <vinodkumarsa987@gmail.com>
2026-08-24 10:20:47 +0530

    tasks day5

---
+++ day_5/program12.java
@@ -0,0 +1,12 @@
+public class program12{
+    public static void main(String[] args) {
+        String s1 ="Hello";
+        String s2= s1;
+        s1=s2+"World";
+        System.out.println("S1= "+s1);
+        System.out.println("S2= "+s2);
+        System.out.println(s1==s2);
+    }
+
+
+}
