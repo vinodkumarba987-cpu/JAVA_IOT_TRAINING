e809b62da2f2976db552104d9cf74c7c9ae28730
vinodkumarba987-cpu <vinodkumarsa987@gmail.com>
2026-08-24 10:10:54 +0530

    day 5 tasks !

---
+++ day_5/program11.java
@@ -0,0 +1,8 @@
+public class program11 {
+    public static void main(String[] args) {
+        String name ="VINOD ";
+        String s1 =name.concat("KUMAR");
+        System.out.println(name);
+        System.out.println(s1);
+    }
+}
