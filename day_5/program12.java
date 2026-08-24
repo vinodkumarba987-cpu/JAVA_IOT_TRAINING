3e05858a70a7be39f4e9107ef03754ba95c0a77f
VINOD KUMAR B A <vinodkumarsa987@gmail.com>
2026-08-24 10:40:49 +0530

    day 5 task done !

---
+++ day_5/program12.java
@@ -0,0 +1,16 @@
+public class program12{
+    public static void main(String[] args) {
+        String s1 ="Hello";
+        String s2= s1;
+        String s3 =new String("Hello !");
+        String s4 = new String("world !");
+        s1=s2+"World";
+        System.out.println("S1= "+s1);
+        System.out.println("S2= "+s2);
+        System.out.println(s3.equals(s4));
+        System.out.println(s3==s4);
+        System.out.println(s1==s2);
+    }
+
+
+}
