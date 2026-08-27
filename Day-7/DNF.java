eb9660c280f9d4384dd92494a4a314d2dad9bdfa
vinodkumarba987-cpu <vinodkumarsa987@gmail.com>
2026-08-27 16:24:10 +0530

    Day-7 task Done !

---
+++ Day-7/DNF.java
@@ -0,0 +1,41 @@
+public class DNF {
+    public static void main(String[] args) {
+
+        int[] a = {2, 0, 1, 2, 1, 0};
+
+        int low = 0;
+        int mid = 0;
+        int high = a.length - 1;
+
+        while (mid <= high) {
+
+            if (a[mid] == 0) {
+
+                int temp = a[low];
+                a[low] = a[mid];
+                a[mid] = temp;
+
+                low++;
+                mid++;
+            }
+
+            else if (a[mid] == 1) {
+
+                mid++;
+            }
+
+            else {
+
+                int temp = a[mid];
+                a[mid] = a[high];
+                a[high] = temp;
+
+                high--;
+            }
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+}
