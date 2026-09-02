c10b2cbafa073ce387aabe92521e49c32b19d535
vinodkumarba987-cpu <vinodkumarsa987@gmail.com>
2026-09-02 15:25:21 +0530

    Day 11 task done !

--- Day-11/SearchMatrix_alphabet.java
+++ Day-11/SearchMatrix_alphabet.java
@@ -1,17 +1,18 @@
-public class SearchMatrix {
+public class SearchMatrix_alphabet {
     public static void main(String[] args) {
 
-        int[][] a = {
-            {10, 20, 30, 40},
-            {15, 25, 35, 45},
-            {27, 29, 37, 48},
-            {50, 55, 60, 70}
+        char[][] a = {
+            {'A','B','C','D'},
+            {'E','F','G','H'},
+            {'I','J','K','L'},
+            {'M','N','O','P'}
         };
 
-        int key = 37;
-        int i = 0, j = 2;
+        char key = 'K';
 
-        while (i < 3 && j >= 0) {
+        int i = 0, j = 3;
+
+        while (i < 4 && j >= 0) {
 
             if (a[i][j] == key) {
                 System.out.println("Found");
