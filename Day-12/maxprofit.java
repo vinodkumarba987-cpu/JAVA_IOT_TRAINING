6166884e1d8a5b27f06300a3e41f37afe692bec0
vinodkumarba987-cpu <vinodkumarsa987@gmail.com>
2026-09-03 16:52:13 +0530

    task day 12

--- Day-12/maxprofit.java
+++ Day-12/maxprofit.java
@@ -5,22 +5,13 @@ public class maxprofit {
 
         int min = price[0];
         int profit = 0;
-        int buy = 0, sell = 0;
 
         for (int i = 1; i < price.length; i++) {
-
-            if (price[i] - min > profit) {
-                profit = price[i] - min;
-                sell = i;
-                buy = 0;
-            }
-
             if (price[i] < min)
                 min = price[i];
+            if (price[i] - min > profit)
+                profit = price[i] - min;
         }
-
         System.out.println("Max Profit = " + profit);
-        System.out.println("Buy Day = " + (buy + 1));
-        System.out.println("Sell Day = " + (sell + 1));
     }
 }
